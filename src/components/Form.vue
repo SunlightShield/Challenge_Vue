6a9c3b559cfbb79d5e1e838ca6248351d307beb5
SunlightShield <sespinozahenriquez@gmail.com>
2024-05-08 13:18:04 -0400

    ya se obtiene el dividendo mensual, falta los otros 2 componentes y toast para validar

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -11,22 +11,63 @@ const props = defineProps({
   }
 })
 
-let time = ref(0) //plazo capturado del formulario
+const time = ref(0) //plazo capturado del formulario
+const totalValue = ref(0) //valor de la propiedad capturado del formulario
+const initialPayment = ref(0) //pie capturado del formulario
+const Interest = ref('5') //interes capturado del formulario
 
 const CapturarDatos = () => {
   console.log('Time:', time.value)
+  console.log('Valor Total:', totalValue.value)
+  console.log('Pago Inicial:', initialPayment.value)
+  console.log('Interest:', Interest.value)
+
+  const dividendoHipotecario = calcularDividendoHipotecario(
+    time.value,
+    totalValue.value,
+    initialPayment.value,
+    Interest.value
+  )
+  console.log('Dividendo Hipotecario:', dividendoHipotecario)
 }
+
+const calcularDividendoHipotecario = (time, totalValue, initialPayment, Interest) => {
+  //parseo todos los datos para obligarlos a ser numeros
+  const timeParsed = parseInt(time)
+  const totalValueParsed = parseInt(totalValue)
+  const initialPaymentParsed = parseInt(initialPayment)
+  const interestParsed = parseInt(Interest)
+
+  //verificar que tanto plazo, valor y pie no sean 0
+  if (timeParsed === 0 || totalValueParsed === 0 || initialPaymentParsed === 0) {
+    console.error('los datos no pueden ser ceros')
+  }
+
+  const loan = totalValueParsed - initialPaymentParsed //re resta el pie del total
+  const finalInterest = interestParsed / 100 / 12 // se pasa el interes a decimal, y luego se divide en 12
+  const paymentPerMonth = loan * (finalInterest / (1 - Math.pow(1 + finalInterest, -timeParsed)))
+  console.log('el pago mensual seria de ' + parseInt(paymentPerMonth))
+}
+
+// Llamada inicial a CapturarDatos
+CapturarDatos()
 </script>
 <template>
   <form @submit.prevent="handleSubmit">
     <div class="container">
       <div class="mb-3">
         <label for="ValorPorpiedad" class="form-label">Valor Propiedad (en UF)</label>
-        <input type="Number" class="form-control" v-model="ValorPorpiedad" placeholder="100000" />
+        <input type="Number" class="form-control" v-model="totalValue" placeholder="100000" />
       </div>
       <div class="mb-3">
         <label for="Pie" class="form-label">Pie (en UF)</label>
-        <input type="Number" class="form-control" id="Pie" v-model="Pie" placeholder="100000" />
+        <input
+          type="Number"
+          class="form-control"
+          id="Pie"
+          v-model="initialPayment"
+          placeholder="100000"
+        />
       </div>
       <div class="mb-3">
         <label for="Interes" class="form-label">Tasa de Interés (por defecto 5%)</label>
@@ -34,7 +75,7 @@ const CapturarDatos = () => {
           type="Number"
           class="form-control disabled"
           id="tasaInteres"
-          v-model="Interes"
+          v-model="Interest"
           placeholder="5%"
         />
       </div>
